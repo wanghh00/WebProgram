be7e73f14f08cd2e1edba6173fd6ac81bf2177f4
Honghao Wang <hongwang@ebay.com>
2014-02-21 18:53:32 -0500

    new file:   TestQeDiag.html 	new file:   qediag.js

---
+++ JS/qediag.js
@@ -0,0 +1,15 @@
+var objDiag = {
+	lstAjaxReq: [],
+	lstJsErr: [],
+	xmlhttprequest_open: XMLHttpRequest.prototype.open,
+};
+
+XMLHttpRequest.prototype.open = function(method,url) {
+	objDiag.xmlhttprequest_open.apply(this, arguments);
+	objDiag.lstAjaxReq.push(method + ' ' + url);
+}
+
+window.onerror = function(msg, url, line_num) {
+	objDiag.lstJsErr.push(msg + ' <' + url + ':' + line_num+'>');
+	return false;
+}
