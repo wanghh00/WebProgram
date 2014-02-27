0c2506f45f900c373123d4caf57239859764e172
Honghao Wang <hongwang@ebay.com>
2014-02-27 13:04:06 -0500

    Add JSONP support

--- JS/qediag.js
+++ JS/qediag.js
@@ -1,15 +1,22 @@
-var objDiag = {
-	lstAjaxReq: [],
-	lstJsErr: [],
-	xmlhttprequest_open: XMLHttpRequest.prototype.open,
+window.objDiag = {
+    lstAjaxReq: [],
+    lstJsErr: [],
+    xmlhttprequest_open: XMLHttpRequest.prototype.open,
+    jsonp_req: function (event) {
+        if (event.target.nodeName == 'SCRIPT' && event.target.src) {
+            objDiag.lstAjaxReq.push('jsonp ' + event.target.src);
+        }
+    }
 };
 
 XMLHttpRequest.prototype.open = function(method,url) {
-	objDiag.xmlhttprequest_open.apply(this, arguments);
-	objDiag.lstAjaxReq.push(method + ' ' + url);
+    objDiag.xmlhttprequest_open.apply(this, arguments);
+    objDiag.lstAjaxReq.push(method + ' ' + url);
 }
 
 window.onerror = function(msg, url, line_num) {
-	objDiag.lstJsErr.push(msg + ' <' + url + ':' + line_num+'>');
-	return false;
+    objDiag.lstJsErr.push(msg + ' <' + url + ':' + line_num+'>');
+    return false;
 }
+window.addEventListener('DOMNodeInserted', objDiag.jsonp_req, false);
+
